9b2986939bf9760288ade877c352ade047c2793f
Barry Bolton <barry@safeviate.com>
2025-07-23 18:06:57 +0000

    So add a button in the left menu like you did yesterday

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -73,7 +73,7 @@ export default function Nav() {
 
   const handleLogout = () => {
     logout();
-    router.push('/corporate');
+    router.push('/login');
   };
 
   const handleLinkClick = () => {
