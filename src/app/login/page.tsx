0307273364608e298759c6701b497f39fc302542
Barry Bolton <barry@safeviate.com>
2025-07-23 15:34:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/login/page.tsx
+++
@@ -1,5 +0,0 @@
-// This page is intentionally left blank to resolve a routing conflict.
-// The correct login page is located at /src/app/(full-page)/login/page.tsx
-export default function BlankPage() {
-  return null;
-}
