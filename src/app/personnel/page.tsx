ad12219bc211436f93a3fac34570fe8ed2eb8f2e
Barry Bolton <barry@safeviate.com>
2025-07-27 20:19:39 +0000

    clear all the cards from the personal page and associated data

--- src/app/personnel/page.tsx
+++ src/app/personnel/page.tsx
@@ -3,7 +3,6 @@
 
 import { useState, useEffect } from 'react';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { useUser } from '@/context/user-provider';
 import { useRouter } from 'next/navigation';
 
@@ -27,15 +26,7 @@ function PersonnelPage() {
 
   return (
       <main className="flex-1 p-4 md:p-8 space-y-8">
-        <Card>
-          <CardHeader>
-              <CardTitle>Personnel Roster</CardTitle>
-              <CardDescription>A list of all non-student personnel in the system.</CardDescription>
-          </CardHeader>
-          <CardContent>
-            {/* Content will be rebuilt here */}
-          </CardContent>
-        </Card>
+        {/* The personnel page content will be built here. */}
       </main>
   );
 }
