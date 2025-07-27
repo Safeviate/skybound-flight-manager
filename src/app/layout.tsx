f4b7bed28b5311a8c424910e9ff27953593f27a6
Barry Bolton <barry@safeviate.com>
2025-07-27 11:59:39 +0000

    is there anythinf slowing the website down?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,13 @@ import { ThemeProvider } from '@/components/theme-provider';
 import { SettingsProvider } from '@/context/settings-provider';
 import { DynamicThemeApplicator } from '@/components/dynamic-theme-applicator';
 import { AppContent } from './app-content';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
 
 
 export const metadata: Metadata = {
@@ -20,12 +27,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={inter.variable}>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
