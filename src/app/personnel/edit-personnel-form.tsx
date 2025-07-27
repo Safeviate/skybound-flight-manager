1ea38e222c58fd7e25ea997a432a291eccd36218
Barry Bolton <barry@safeviate.com>
2025-07-27 21:24:26 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <FormItem classNam

--- src/app/personnel/edit-personnel-form.tsx
+++ src/app/personnel/edit-personnel-form.tsx
@@ -174,44 +174,6 @@ export function EditPersonnelForm({ personnel, onSubmit }: EditPersonnelFormProp
             )}
             />
             
-            <FormField
-                control={form.control}
-                name="licenseExpiry"
-                render={({ field }) => (
-                    <FormItem className="flex flex-col">
-                        <FormLabel>Pilot License Expiry</FormLabel>
-                        <Popover>
-                            <PopoverTrigger asChild>
-                            <FormControl>
-                                <Button
-                                variant={"outline"}
-                                className={cn(
-                                    "w-full pl-3 text-left font-normal",
-                                    !field.value && "text-muted-foreground"
-                                )}
-                                >
-                                {field.value ? (
-                                    format(field.value, "PPP")
-                                ) : (
-                                    <span>Pick expiry date</span>
-                                )}
-                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                                </Button>
-                            </FormControl>
-                            </PopoverTrigger>
-                            <PopoverContent className="w-auto p-0" align="start">
-                            <Calendar
-                                mode="single"
-                                selected={field.value}
-                                onSelect={field.onChange}
-                                initialFocus
-                            />
-                            </PopoverContent>
-                        </Popover>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
         </div>
 
         <Separator />
