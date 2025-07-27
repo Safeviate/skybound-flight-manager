ea7a09f9b298463d3da4ed55c3973bb3e7cc4c2f
Barry Bolton <barry@safeviate.com>
2025-07-27 16:00:14 +0000

    Fuck me nooooooooooo remove it

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -42,37 +42,12 @@ export default function Header({ title, children }: { title: string, children?: 
       
       <div className="flex items-center justify-end gap-4">
         {children}
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="relative h-10 w-10 rounded-full">
-                    <Avatar className="h-10 w-10">
-                         <AvatarImage src={`https://placehold.co/40x40.png`} alt={user?.name} data-ai-hint="user avatar" />
-                        <AvatarFallback>{userInitial}</AvatarFallback>
-                    </Avatar>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56" align="end" forceMount>
-                <DropdownMenuLabel className="font-normal">
-                    <div className="flex flex-col space-y-1">
-                        <p className="text-sm font-medium leading-none">{user?.name}</p>
-                        <p className="text-xs leading-none text-muted-foreground">
-                        {user?.email}
-                        </p>
-                    </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem asChild>
-                    <Link href="/my-profile">
-                        <UserIcon className="mr-2 h-4 w-4" />
-                        <span>Profile</span>
-                    </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={handleLogout}>
-                    <LogOut className="mr-2 h-4 w-4" />
-                    <span>Log out</span>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <Button variant="ghost" size="icon" asChild>
+          <Link href="/my-profile">
+            <UserIcon />
+            <span className="sr-only">My Profile</span>
+          </Link>
+        </Button>
       </div>
     </header>
   );
