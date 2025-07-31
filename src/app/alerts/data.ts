70985175a931b59ce68cb408392c528ff57cc15a
Barry Bolton <barry@safeviate.com>
2025-07-31 09:55:19 +0000

    Answer only. How is that fixed?

---
+++ src/app/alerts/data.ts
@@ -0,0 +1,16 @@
+
+'use server';
+
+import { db } from '@/lib/firebase';
+import { collection, query, getDocs, orderBy } from 'firebase/firestore';
+import type { Alert } from '@/lib/types';
+
+export async function getAlertsPageData(companyId: string) {
+    const alertsQuery = query(collection(db, `companies/${companyId}/alerts`), orderBy('date', 'desc'));
+    
+    const alertsSnapshot = await getDocs(alertsQuery);
+
+    const alertsList = alertsSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() } as Alert));
+    
+    return { alertsList };
+}
