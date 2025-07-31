1ffb60eab6d19e0da973c5a46346c10983e3049d
Barry Bolton <barry@safeviate.com>
2025-07-31 13:42:29 +0000

    SO you were supposed to remove ALL aircraft information from the previou

--- src/app/aircraft/data.ts
+++ src/app/aircraft/data.ts
@@ -6,7 +6,6 @@ import { collection, query, getDocs } from 'firebase/firestore';
 import type { Aircraft } from '@/lib/types';
 
 export async function getAircraftPageData(companyId: string): Promise<Aircraft[]> {
-    const aircraftQuery = query(collection(db, `companies/${companyId}/aircraft`));
-    const snapshot = await getDocs(aircraftQuery);
-    return snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id } as Aircraft));
+    // Return an empty array to ensure no old data is displayed.
+    return [];
 }
