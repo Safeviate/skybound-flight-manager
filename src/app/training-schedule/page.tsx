77ff779f33404c7a8bf4af01f457523f9b7ea771
Barry Bolton <barry@safeviate.com>
2025-08-01 15:56:10 +0000

    The training schedule must be void of any reference data and formatting.

--- src/app/training-schedule/page.tsx
+++ src/app/training-schedule/page.tsx
@@ -1,23 +1,7 @@
 'use client';
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-
 function TrainingSchedulePage() {
-  return (
-    <main className="flex-1 p-4 md:p-8">
-      <Card>
-        <CardHeader>
-          <CardTitle>Training Schedule</CardTitle>
-          <CardDescription>
-            This page will display the training schedule.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          {/* Content will be added here */}
-        </CardContent>
-      </Card>
-    </main>
-  );
+  return null;
 }
 
 TrainingSchedulePage.title = 'Training Schedule';
