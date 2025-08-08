d85de7f8ef9cfc7b60e66557ae54c724e6702da1
Barry Bolton <barry@safeviate.com>
2025-08-08 12:44:06 +0000

    Ammended version number to 0.1.2

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,1 @@
-import developmentConfig from './development';
-import betaConfig from './beta';
-import { AppConfig } from './types';
 
-const useMockData = process.env.NEXT_PUBLIC_USE_MOCK_DATA === 'true';
-
-const config = useMockData ? developmentConfig : betaConfig;
-
-export default {
-    ...config,
-    useMockData,
-} as AppConfig;
