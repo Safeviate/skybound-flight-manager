b8746506b09b74f3f22fe4be7edeef9c6b559bc3
Barry Bolton <barry@safeviate.com>
2025-08-17 13:30:46 +0000

    the date and time picker in the bookings window need to be increased to

--- src/app/training-schedule/new-booking-form.tsx
+++ src/app/training-schedule/new-booking-form.tsx
@@ -96,8 +96,8 @@ export function NewBookingForm({ aircraft, users, bookings, onSubmit, onDelete, 
   const [otherReason, setOtherReason] = useState('');
 
   const timeSlots = useMemo(() => {
-    return Array.from({ length: (24 - 6) * 4 }, (_, i) => {
-        const hour = Math.floor(i / 4) + 6;
+    return Array.from({ length: 24 * 4 }, (_, i) => {
+        const hour = (Math.floor(i / 4) + 6) % 24;
         const minute = (i % 4) * 15;
         return `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
     });
