374412839737816dd548370e7b9a322ac43a7609
Barry Bolton <barry@safeviate.com>
2025-08-17 21:35:43 +0000

    A component is changing an uncontrolled input to be controlled. This is

--- src/app/training-schedule/new-booking-form.tsx
+++ src/app/training-schedule/new-booking-form.tsx
@@ -84,6 +84,9 @@ export function NewBookingForm({ aircraft, users, bookings, onSubmit, onDelete, 
     defaultValues: {
       startTime: '',
       endTime: '',
+      departure: '',
+      arrival: '',
+      trainingExercise: '',
     }
   });
 
@@ -92,7 +95,7 @@ export function NewBookingForm({ aircraft, users, bookings, onSubmit, onDelete, 
 
   const timeSlots = useMemo(() => {
     return Array.from({ length: 24 * 4 }, (_, i) => {
-        const hour = (Math.floor(i / 4)) % 24;
+        const hour = (Math.floor(i / 4) + 6) % 24;
         const minute = (i % 4) * 15;
         return `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
     });
