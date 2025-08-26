424bebc6e3dd172933fda99dc1bbb50714cd110c
Barry Bolton <barry@safeviate.com>
2025-08-26 13:28:27 +0000

    This is in the console of the MOC

--- src/next.config.ts
+++ src/next.config.ts
@@ -6,7 +6,7 @@ const pwaConfig = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development' || process.env.PWA_DISABLED === 'true',
+  disable: true,
   runtimeCaching: [
     {
       urlPattern: /^https?.*/,
