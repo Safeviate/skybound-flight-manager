b67cbfdce394c51322968cfd875d2f40f675f800
Barry Bolton <barry@safeviate.com>
2025-11-27 20:42:29 +0000

    implement and lets see if this works

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+  },
   async headers() {
     return [
       {
