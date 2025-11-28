05e8bc92a47dfa2ce26590fa3580e28abb70e876
Barry Bolton <barry@safeviate.com>
2025-11-28 09:13:50 +0000

    Can i add it manually?

--- src/next.config.ts
+++ src/next.config.ts
@@ -2,6 +2,7 @@
 import type {NextConfig} from 'next';
 import withPWA from 'next-pwa';
 
+// This comment is added to re-trigger the deployment process.
 const pwaConfig = withPWA({
   dest: 'public',
   register: true,
