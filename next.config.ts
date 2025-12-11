305e931ae533419057a0d4cfa99228107759153a
Barry Bolton <barry@safeviate.com>
2025-12-11 11:31:13 +0000

    i dot knowunning "npm ci --quiet --no-fund --no-audit (NODE_ENV=developm

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   // This is a trivial comment to force a recompilation and fix chunk loading errors.
-  webpack: (config, { isServer }) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'handlebars': 'handlebars/dist/cjs/handlebars.js',
-    }
-    return config
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
